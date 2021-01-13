f1e9e318ebfe2b3902c51e7df73280e11b41176d
satish-shah <fireside.satish2012@gmail.com>
2021-01-14 09:01:44 +1100

    First Commit

---
+++ src/components/Pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is the Todo App v1.0.0. This is part of React Crash Course</p>
+            
+        </React.Fragment>
+    )
+}
+
+export default  About;
